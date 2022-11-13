cc1d7cfef8e1e0f0e487c2f4577ca08b71ab010c
Dylan Solomon <dylansolomon413@gmail.com>
2022-11-13 13:48:56 -0800

    Enable new book to be displayed in library

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ const modal = document.getElementById('modal-container');
 const btn = document.getElementById('btn-add');
 const push = document.getElementsByClassName('btn-push')[0]
 const close = document.getElementsByClassName('close')[0];
+const form = document.getElementsByClassName('modal-content')[0]
+
 
 btn.addEventListener('click', modalPopup);
 close.addEventListener('click', modalPopup);
-window.addEventListener('click', modalClose)
+window.addEventListener('click', modalClose);
+push.addEventListener('click', pushToLibrary);
 
 function modalPopup(){
     if(modal.classList.contains('none')){
         modal.classList.toggle('none');
         modal.style.display = 'block';
     } else {
-        modal.style.display = 'none';
-        modal.classList.toggle('none');
+        hideModal()
     }
 }
 
 function modalClose(e) {
     if(e.target == modal) {
-        modal.style.display = 'none';
-        modal.classList.toggle('none')
+        hideModal()
     }
 }
 
@@ -38,8 +39,7 @@ function Book(title, author, read) {
     this.read = read
 }
 
-
-function addToLibrary() {
+function initialLibrary() {
     for(let i = 0; i <myLibrary.length; i++){
         let row = document.createElement('tr')
         let title = document.createElement('td')
@@ -55,4 +55,31 @@ function addToLibrary() {
     }
 }
 
-addToLibrary()
+function addToLibrary() {
+    let row = document.createElement('tr')
+    let title = document.createElement('td')
+    let author = document.createElement('td')
+    let read = document.createElement('td')
+    title.innerText = `${myLibrary[myLibrary.length-1].title}`
+    author.innerText = `${myLibrary[myLibrary.length-1].author}`
+    read.innerText = `${myLibrary[myLibrary.length-1].read}`
+    row.appendChild(title)
+    row.appendChild(author)
+    row.appendChild(read)
+    tBody.appendChild(row)
+}
+
+function pushToLibrary(e) {
+    e.preventDefault()
+    myLibrary[myLibrary.length] = new Book(form.title.value, form.author.value, form.read.value)
+    addToLibrary()
+    hideModal()
+    form.reset()  
+}
+
+function hideModal() {
+    modal.style.display = 'none';
+    modal.classList.toggle('none')
+}
+
+initialLibrary()
