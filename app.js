a2e7154c42a90db52a04b77b538ea06bb847ef88
Dylan Solomon <dylansolomon413@gmail.com>
2022-11-13 18:35:10 -0800

    Fix bug to update table. MVP complete

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const push = document.getElementsByClassName('btn-push')[0]
 const close = document.getElementsByClassName('close')[0];
 const form = document.getElementsByClassName('modal-content')[0];
 const remove = document.querySelectorAll('.delete');
-const rows = tBody.querySelectorAll('tr')
-
-
 let myLibrary = [];
 
 btn.addEventListener('click', modalPopup);
@@ -19,7 +16,7 @@ remove.forEach(rem => {
     rem.addEventListener('click', deleteBook)
 })
 
-
+//MODAL FORM FUNCTIONS
 function modalPopup(){
     if(modal.classList.contains('none')){
         modal.classList.toggle('none');
@@ -35,10 +32,9 @@ function modalClose(e) {
     }
 }
 
-function Book(title, author, read) {
-    this.title = title
-    this.author = author
-    this.read = read
+function hideModal() {
+    modal.style.display = 'none';
+    modal.classList.toggle('none')
 }
 
 function addToLibrary() {
@@ -61,7 +57,6 @@ function addToLibrary() {
     row.appendChild(title)
     row.appendChild(author)
 
-    //del.classList.add(`${title.innerText}`)
     if(`${myLibrary[myLibrary.length-1].read}` == 'true'){
         hasRead.setAttribute('checked', 'true');
         read.appendChild(hasRead)
@@ -75,9 +70,16 @@ function addToLibrary() {
     row.appendChild(deleteMe)
 
     tBody.appendChild(row)
-
 }
 
+//OBJECT CONSTRUCTOR
+function Book(title, author, read) {
+    this.title = title
+    this.author = author
+    this.read = read
+}
+
+//ARRAY UPDATE FUNCTIONS
 function pushToLibrary(e) {
     if(form.checkValidity()){
         e.preventDefault()
@@ -88,37 +90,56 @@ function pushToLibrary(e) {
     }
 }
 
-function hideModal() {
-    modal.style.display = 'none';
-    modal.classList.toggle('none')
-}
-
 function deleteBook(e){
     let rowJ = e.target.parentElement.parentElement;
     rowJ.classList.add(`${rowJ.rowIndex}`)
     let rowIndex = rowJ.classList[0]
     myLibrary.splice(Number(rowIndex)-1,1);
-
-    // let row = e.target.classList[1]-1
-    // myLibrary.splice((e.target.classList[1])-1,1);
-    //  resetTable()
-     
-    // initialLibrary()
+    resetTable()
+    initialLibrary()
 }
 
+//TABLE UPDATE FUNCTIONS
+function initialLibrary() {
+    for(let i = 0; i <myLibrary.length; i++){
+        let row = document.createElement('tr')
+        let title = document.createElement('td')
+        let author = document.createElement('td')
+        let read = document.createElement('td')
+        let deleteMe = document.createElement('td')
+        let hasRead = document.createElement('input')
+        hasRead.setAttribute('type', 'checkbox');
+        let del = document.createElement('input');
+        del.setAttribute('type', 'button')
+        del.setAttribute('value', 'Delete')
+        del.classList.add('delete')
+    
+        del.addEventListener('click', deleteBook)
+    
+        title.innerText = `${myLibrary[i].title}`
+        author.innerText = `${myLibrary[i].author}`
+        row.appendChild(title)
+        row.appendChild(author)
+    
+        if(`${myLibrary[i].read}` == 'true'){
+            hasRead.setAttribute('checked', 'true');
+            read.appendChild(hasRead)
+            row.appendChild(read)
+        } else {
+            read.appendChild(hasRead)
+            row.appendChild(read)
+        }
+    
+        deleteMe.appendChild(del)
+        row.appendChild(deleteMe)
+    
+        tBody.appendChild(row)
+    }
+}
 
-// function initialLibrary() {
-//     for(let i = 0; i <myLibrary.length; i++){
-//         addToLibrary()
-//         console.log(myLibrary)
-//     }
-// }
-
-
-
-// function resetTable(){
-//     let rowCount = table.rows.length;
-//     for(let i = rowCount - 1; i > 0; i--){
-//         table.deleteRow(i)
-//     }
-// }
+function resetTable(){
+    let rowCount = table.rows.length;
+    for(let i = rowCount - 1; i > 0; i--){
+        table.deleteRow(i)
+    }
+}
